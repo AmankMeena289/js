aa0009e57c07f559185ba0ece07a991f81c24d22
AmankMeena289 <amankumarm289@gmail.com>
2026-05-16 09:48:39 +0000

    prettier

---
+++ 01Basic/var.js
@@ -0,0 +1,8 @@
+const accId = 234
+let accEmail = "abc@go.on";
+var accPass = "12121212";
+accCity = "jaipur"
+// no need of semicolon 
+// dont use var( avoid using var);
+
+console.table([accId,accEmail, accPass , accCity])
